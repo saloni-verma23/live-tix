5713c5c7ab45732f757927844711329432963eed
saloni-verma23 <vermasaloni365@gmail.com>
2025-01-06 18:02:02 +0530

    6-Dec-hero

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 import AnimatedText from "./AnimatedText";
 const Main = () => {
   return (
-    <div className="position-relative overflow-hidden p-3 p-md-5 m-2 m-md-3 m-md-3 text-center hero">
+    <div
+      className="position-relative overflow-hidden p-3 p-md-5 m-2 m-md-3 m-md-3 text-center hero"
+      style={{ backgroundImage: "url(hero.jpg)" }}
+    >
       <div className="col-md-6.5 p-lg-5 mx-auto my-5">
         <div>
           <p className="display-10 fw-bold hero-text">
